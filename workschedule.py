d39aa4108040c34239ffe4883bf7fa5ccc64c19b
NicholasNPham <phamn1000@gmail.com>
2025-08-13 13:11:46 -0400

    Added direction for selenium to navigate employee portal first step to second step that contains the username/password input fields

--- workschedule.py
+++ workschedule.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+
+# Website link to Employee Portal (ALDI)
 driver.get("https://myaldius.staffbase.com/content/page/609ea1450e49ad1c940fd1ab")
+
+# First Step
+wait = WebDriverWait(driver, 10)
+buttonOne = wait.until(EC.element_to_be_clickable((By.ID, "public-login-hint")))
+buttonOne.click()
+
+# Second Step, Gain Access to Login Input
+wait = WebDriverWait(driver, 10)
+buttonTwo = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "type-uncollapse")))
+buttonTwo.click()
 
 time.sleep(10)
 
 driver.quit()
+
+
