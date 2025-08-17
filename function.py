26275d4f744791fd946f09ca06978e3e79efba48
Nicholas Pham <nicholaspham@Nicholass-MBP.lan>
2025-08-17 18:54:53 -0400

    Added function to turn date into ISO formatted Date

---
+++ function.py
@@ -0,0 +1,15 @@
+import datetime
+
+def newDate(date):
+    now = datetime.datetime.now()
+    year = now.year
+    if date[1] == '-':
+        date = "0" + date
+    else:
+        date = date
+
+    isoDate = str(year)+ '-' + date
+    return isoDate
+
+stringExample = "1-20"
+print(newDate(stringExample))
