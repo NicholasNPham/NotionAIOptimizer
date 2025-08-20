aba727be8609c3368ca7a3333f1641cc73c43536
Nicholas Pham <nicholaspham@Nicholass-MBP.lan>
2025-08-20 14:05:07 -0400

    optimized newDate function

--- function.py
+++ function.py
@@ -1,26 +1,18 @@
-import datetime
+from datetime import datetime
 
 def newDate(date):
-    now = datetime.datetime.now()
-    year = now.year
-    if date[1] == '-':
-        date = "0" + date
-    else:
-        date = date
-
-    isoDate = str(year)+ '-' + date
+    now = datetime.now()
+    formattingDate = "%m-%d"
+    date = datetime.strptime(date,formattingDate)
+    isoDate = f'{now.year}-{date.strftime("%m-%d")}'
     return isoDate
-
-string = "7.00hr"
 
 def formattedHour(hour):
     removedLetterString = []
     for char in hour:
        if char.isdigit() or char == '.':
            removedLetterString.append(char)
-    print(removedLetterString)
     return "".join(removedLetterString)
 
-newHour = (formattedHour(string))
-print(newHour)
+
 
