65e3c6b8d25caf2c1c606c18a2ba85658bd505e2
NicholasNPham <phamn1000@gmail.com>
2025-08-27 17:06:37 -0400

    called to find each rowID, then zip the two dictionaries together to add to notion database, problem im having right now is that the dictionary is interating backwards do to how notion takes data. the first index of dictSchedule database becomes the first index[0] which is the last rowID

--- main.py
+++ main.py
@@ -23,57 +23,39 @@ testDictionary = {
     "2025-08-26": [['2025-08-26T14:00:00-04:00', '2025-08-26T21:00:00-04:00'], 7.0]
 }
 
-# Finding Valid Page ID
+# Results gets all the database rows in a dictionary
 results = notion.databases.query(database_id=pageID)
-for page in results['results']:
-    validPageID = page['id']
-    print(validPageID)
+# The pages variable stores the list of page objects
+pages = results['results']
 
-for key, value in dictSchedule.items():
-
-    if value[0] == "No Shift Scheduled":
-        shiftProperties = key
-        startTimeProperties, endTimeProperties = None, None
-        hourProperties = value[1]
-    else:
-        shiftProperties = key
-        startTimeProperties = value[0][0]
-        endTimeProperties = value[0][1]
-        hourProperties = value[1]
-
-    textProperty = [{"text": {"content": shiftProperties}}]
-    if startTimeProperties == None and endTimeProperties == None:
-        dateProperty = {"date": None}
-    else:
-        dateProperty = {"date": {"start": startTimeProperties, "end": endTimeProperties}}
-    hourProperty = {"number": hourProperties}
-
-    properties = {
-        "Shift": textProperty,
-        "Start Time": dateProperty,
-        "Hours": hourProperty
-    }
+# Zip called to pair elements from the two dictionaries that turned into lists
+for page, (key, value) in zip(pages, dictSchedule.items()):
+    rowID = page['id']
+    textProperty = {"Shift": {"title": [{"text": {"content": key}}]}}
 
     # Try Block to update database "Work Schedule Database"
     try:
-        response = notion.pages.update(page_id=validPageID, properties=dateProperty)
+        response = notion.pages.update(page_id=rowID, properties=textProperty)
         print("page updated", response)
     except Exception as e:
-        print(f'error updating page {e}')
+        print(e)
+
+# This is CODE HELL VVVV WILL BE CHANGED
+
+    # if value[0] == "No Shift Scheduled":
+    #     startTimeProperties, endTimeProperties = None, None
+    #     hourProperties = value[1]
+    # else:
+    #     startTimeProperties = value[0][0]
+    #     endTimeProperties = value[0][1]
+    #     hourProperties = value[1]
+
+    # if startTimeProperties == None and endTimeProperties == None:
+    #     dateProperty = {"date": None}
+    # else:
+    #     dateProperty = {"date": {"start": startTimeProperties, "end": endTimeProperties}}
+    # hourProperty = {"number": hourProperties}
+    #
+    # properties = {"Shift": textProperty, "Start Time": dateProperty, "Hours": hourProperty}
 
 
-"""
-
-    Research:
-    First point: Data is updated creating the database with 14 entries
-        - once the second monday has been reach, remove previous 7 days and update with the next 7 days
-    
-    Second point: what happens if any day has been updating or shift has been change?
-        - then i need to have a checker function: if no shift has been set, remove no shift and add shift,
-        - if shift is already present, remove shift start and end time and update new shift start and end time.
-
-    Seems easy enough being typed, need to learn notion api and how to update, remove times.
-    also i want to learn about branching from main to learn about about github repository and learning to actually SWE
-    big big project, class as started so project will inevitably slow down. But project is to fun to stop now.
-
-"""
