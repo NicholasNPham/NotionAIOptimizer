baee96620398db80e1f4517cb39c893aae1e09ec
NicholasNPham <phamn1000@gmail.com>
2025-08-30 22:38:39 -0400

    created new variable propertyDict to update database with 3 properties per line

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ response = notion.databases.query(database_id=database_id)
 # Print the results, Uncomment to print
 for page in response['results']:
     properties = page['properties']
-    # print(properties)
+    print(properties)
 
 # Results gets all the database rows in a dictionary
 results = notion.databases.query(database_id=pageID)
@@ -27,20 +27,20 @@ pages = results['results']
 for page, (key, value) in zip(pages, reversedDictSchedule.items()):
     rowID = page['id']
     # Format dictionary of shift date as text in NOTION API SCHEMA
-    textProperty = {"Shift": {"title": [{"text": {"content": key}}]}}
+    textProperty = {"title": [{"text": {"content": key}}]}
     # Format dictionary of hours scheduled in NOTION API SCHEMA
-    hourProperty = {"Hour": {"number": value[1]}}
+    hourProperty = {"number": value[1]}
     # Format dictionary of shift time in NOTION API SCHEMA
     if value[0] == "No Shift Scheduled":
-        dateProperty = {"Date": {"date": None}}
+        dateProperty = {"date": None}
     else:
-        dateProperty = {"Date": {"date": {"start": value[0][0], "end": value[0][1]}}}
-
+        dateProperty = {"date": {"start": value[0][0], "end": value[0][1]}}
     # Code Below will be to create a variable containing text,hour,date property to update 3 in 1 per 1 in 14 days
+    propertyDict = {"Hour": hourProperty, "Date": dateProperty, "Shift": textProperty}
 
     # Try Block to update database "Work Schedule Database"
     try:
-        response = notion.pages.update(page_id=rowID, properties=dateProperty)
+        response = notion.pages.update(page_id=rowID, properties=propertyDict)
         print("page updated", response)
     except Exception as e:
         print(e)
