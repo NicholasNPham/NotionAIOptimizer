18c305bb7a4d1e389c90c40deafdf656628fd603
NicholasNPham <phamn1000@gmail.com>
2025-09-06 23:55:11 -0400

    fixed syntax

--- daemon.py
+++ daemon.py
@@ -8,4 +8,4 @@ This is created with Tkinter for now, then learn how to make an application on d
     display button run and quit
     when run, open command prompt and type a run command of daemon file that runs the main.py file to run on logging on
     we can first try at open however, I dont want to have the pc turn on in the early morning. so push for on log on.
-""""""
+"""
