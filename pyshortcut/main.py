22c82e3f46c5f406698a193125f11aa03f138a6d
NicholasNPham <phamn1000@gmail.com>
2025-09-17 15:47:26 -0400

    successfully created 'shortcut' need to work on icon

--- pyshortcut/main.py
+++ pyshortcut/main.py
@@ -3,8 +3,9 @@ import win32com.client
 # Testing Functionality
 shell = win32com.client.Dispatch("WScript.Shell")
 shortcut = shell.CreateShortCut( r"C:\Users\phamn\OneDrive\Desktop\DAEMON.lnk")
-shortcut.TargetPath = r"E:\PycharmProjects\NotionAIOptimizer\DAEMON\daemon.py"
-shortcut.WorkingDirectory = r"C:\Users\phamn\OneDrive\Desktop"
+shortcut.TargetPath = r"C:\Users\phamn\AppData\Local\Programs\Python\Launcher\pyw.exe" # Change between py.exe for show and pyw.exe to not show
+shortcut.Arguments = r"E:\PycharmProjects\NotionAIOptimizer\DAEMON\interface.py"
+shortcut.WorkingDirectory = r"E:\PycharmProjects\NotionAIOptimizer\DAEMON"
 shortcut.save()
 
 # Main Function to Run
