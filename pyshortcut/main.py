0576f9206803dea6e0ea49c2c74927c9ca83b6c7
NicholasNPham <phamn1000@gmail.com>
2025-09-17 16:23:01 -0400

    functionality successful

--- pyshortcut/main.py
+++ pyshortcut/main.py
@@ -6,17 +6,20 @@ shortcut = shell.CreateShortCut( r"C:\Users\phamn\OneDrive\Desktop\DAEMON.lnk")
 shortcut.TargetPath = r"C:\Users\phamn\AppData\Local\Programs\Python\Launcher\pyw.exe" # Change between py.exe for show and pyw.exe to not show
 shortcut.Arguments = r"E:\PycharmProjects\NotionAIOptimizer\DAEMON\interface.py"
 shortcut.WorkingDirectory = r"E:\PycharmProjects\NotionAIOptimizer\DAEMON"
+shortcut.IconLocation = r'E:\PycharmProjects\NotionAIOptimizer\DAEMON\samurai.ico'
 shortcut.save()
 
 # Main Function to Run
 def createPythonShortcut(path=None, name=None, icon=None, terminal=None):
 
     shell = win32com.client.Dispatch("WScript.Shell")
+
     lnkFileLocation = rf"{shell.SpecialFolders("Desktop")}" + rf"\{name}.lnk"
+
+    iconFile = icon
 
     pathDirectory = path
     desktopName = name
-    iconFile = icon
     terminalBool = terminal
     return lnkFileLocation
 
